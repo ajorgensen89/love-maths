69b09e0c198edc0cbfef7b446f874dc3c6c09a84
Ava Jorgensen <115990399+ajorgensen89@users.noreply.github.com>
2022-12-05 14:01:09 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+function runGame() {
+
+}
+function checkAnswer() {
+    
+}
+function calculateCorrectAnswer() {
+
+}
+function incrementScore() {
+
+}
+function incrementWrongAnswer() {
+
+}
+function displayAdditionQuestion() {
+
+}
+function displaySubtractQuestion() {
+
+}
+function displayMultiplyQuestion() {
+
+}
+
