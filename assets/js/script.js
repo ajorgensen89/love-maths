025e8cf62184e5f01f0c292b49a52f497082cbaf
Ava Jorgensen <115990399+ajorgensen89@users.noreply.github.com>
2022-12-08 20:06:47 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,19 +10,27 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+    runGame("addition")
 })
 
 /** The main game 'loop', called when the script is first loaded
  * and after the user's answer has been processed.
  */
-function runGame() {
+function runGame(gameType) {
     // creates 2 random numbers between 1 - 25.
  let num1 = Math.floor(Math.random() * 25) + 1;
  let num2 = Math.floor(Math.random() * 25) + 1;
+
+ if(gameType === "addition") {
+    displayAdditionQuestion(num1, + num2);
+ } else {
+    alert(`unknown game type: ${gameType}`);
+    throw `unknown game type: ${gameType}. Aborting!`;
+ }
 }
 function checkAnswer() {
     
@@ -36,7 +44,10 @@ function incrementScore() {
 function incrementWrongAnswer() {
 
 }
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 
 }
 function displaySubtractQuestion() {
