eb185d8cf510d75df550be4244a31a532bb4d288
ajorgensen89 <ajorgensen89@gmail.com>
2022-12-12 12:39:51 +0000

    Add code for subtraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,7 +29,9 @@ function runGame(gameType) {
     displayAdditionQuestion(num1, num2);
  } else if (gameType === "multiply") {
     displayMultiplyQuestion(num1, num2);
- }else {
+ } else if (gameType === "subtract") {
+    displaySubtractQuestion(num1, num2);
+ } else {
     alert(`unknown game type: ${gameType}`);
     throw `unknown game type: ${gameType}. Aborting!`;
  }
@@ -64,6 +66,8 @@ function calculateCorrectAnswer() {
     return [operand1 + operand2, "addition"];
   } else if (operator === "x") {
     return [operand1 * operand2, "multiply"];
+} else if (operator === "-") {
+    return [operand1 - operand2, "subtract"];
 } else {
     alert(`Unimplemented operator ${operator}`);
     throw `Unimplemented operator ${operator}. Aborting!`;
@@ -90,7 +94,9 @@ function displayAdditionQuestion(operand1, operand2) {
 
 }
 function displaySubtractQuestion(operand1, operand2) {
-    
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
 }
 function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
